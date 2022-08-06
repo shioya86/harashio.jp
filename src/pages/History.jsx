ab702fea6df3d8d2eeb495d288a8cf55443856f8
Shoya HARA <shioyarare@gmail.com>
2022-08-07 01:26:17 +0900

    fix: 変数名

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -5,17 +5,17 @@ import { Timeline, Event } from 'react-trivial-timeline';
 // こっちも使えそう → https://codepen.io/viktorjs/pen/KQZYjo
 function History() {
   const contents =  [
-    {time: "6.2017", title: "宮崎ミライ Hack Days", body: "宮崎の観光をテーマにしたハッカソン"},
-    {time: "10.2017", title: "全国高専プログラミングコンテスト 第28回大島大会 参加", body: "C++ / OpenCVを用いたパズルソルバの開発"},
-    {time: "1.2018", title: "第2回 RESASアプリコンテスト 参加", 
+    {date: "6.2017", title: "宮崎ミライ Hack Days", body: "宮崎の観光をテーマにしたハッカソン"},
+    {date: "10.2017", title: "全国高専プログラミングコンテスト 第28回大島大会 参加", body: "C++ / OpenCVを用いたパズルソルバの開発"},
+    {date: "1.2018", title: "第2回 RESASアプリコンテスト 参加", 
       subtitle: "宮崎地方創成シミュレーションゲーム 輝け☆ミライの観光大使", body: "優秀賞, ソフトバンクテクノロジー賞"},
-    {time: "6.2019", title: "CTF ビギナーズ for 高専生", body: "Offensive Securityの基礎"},
-    {time: "3.2022", title: "電子情報通信学会九州支部成績優秀賞", body: "九州工業大学 情報工学部 情報・通信工学科"},
+    {date: "6.2019", title: "CTF ビギナーズ for 高専生", body: "Offensive Securityの基礎"},
+    {date: "3.2022", title: "電子情報通信学会九州支部成績優秀賞", body: "九州工業大学 情報工学部 情報・通信工学科"},
 
   ]
 
   const drawer =  (contents.map((e) => (
-          <Event interval={e.time} title={e.title} subtitle={e?.subtitle}>
+          <Event interval={e.date} title={e.title} subtitle={e?.subtitle}>
             {e.body}
           </Event>
         ))
