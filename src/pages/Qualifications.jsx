b66d53a9a9257844bd1a3d1f3853b98afdfbd838
Shoya HARA <shioyarare@gmail.com>
2022-08-07 01:23:23 +0900

    feat: 資格情報をいい感じに

--- src/pages/Qualifications.jsx
+++ src/pages/Qualifications.jsx
@@ -1,15 +1,28 @@
 import Title from "../components/Title";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 function Qualifications() {
+  const rows = [
+    {date: "9.2018", name: "ITパスポート"},
+    {date: "5.2019", name: "基本情報技術者資格"},
+    {date: "3.2020", name: "準学士(工学) - 都城工業高等専門学校"},
+    {date: "3.2022", name: "学士(情報工学) - 九州工業大学"}
+  ]
   return (
     <div>
       <Title name={"Qualifications"} />
-      <ul>
-        <li>ITパスポート</li>
-        <li>基本情報技術者資格</li>
-        <li>準学士(工学) - 都城工業高等専門学校</li>
-        <li>学士(情報工学) - 九州工業大学</li>
-      </ul>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+        {
+          rows.map( (e) => (
+            <ListItem>
+              <ListItemText primary={e.name} secondary={e.date} />
+            </ListItem>
+          ))
+        }
+      </List>
     </div>
   );
 }
