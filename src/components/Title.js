5497dd895f68f821398f96529f54fb325220dcb4
Shoya HARA <shara@ml.harashio.jp>
2022-09-06 00:43:36 +0900

    styleを揃えよう委員会

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,7 +6,6 @@ const style =  {
   },
   header: {
     paddingTop: 60,
-    borderBottom: "double 3px rgba(25, 118, 210, 0.3"
   }
 
 }
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,34 +1,39 @@
 import Title from "../components/Title";
 import { Timeline, Event } from 'react-trivial-timeline';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 // これを使っている → https://github.com/tverdohleb/react-trivial-timeline
 // こっちも使えそう → https://codepen.io/viktorjs/pen/KQZYjo
 function History() {
-  const contents =  [
-    {date: "6.2017", title: "宮崎ミライ Hack Days", body: "宮崎の観光をテーマにしたハッカソン"},
-    {date: "10.2017", title: "全国高専プログラミングコンテスト 第28回大島大会 参加", body: "C++ / OpenCVを用いたパズルソルバの開発"},
-    {date: "1.2018", title: "第2回 RESASアプリコンテスト 参加", 
+  const rows =  [
+    {date: "6.2017", name: "宮崎ミライ Hack Days", body: "宮崎の観光をテーマにしたハッカソン"},
+    {date: "10.2017", name: "全国高専プログラミングコンテスト 第28回大島大会 参加", body: "C++ / OpenCVを用いたパズルソルバの開発"},
+    {date: "1.2018", name: "第2回 RESASアプリコンテスト 参加", 
       subtitle: "宮崎地方創成シミュレーションゲーム 輝け☆ミライの観光大使", body: "優秀賞, ソフトバンクテクノロジー賞"},
-    {date: "6.2019", title: "CTF ビギナーズ for 高専生", body: "Offensive Securityの基礎"},
-    {date: "3.2022", title: "電子情報通信学会九州支部成績優秀賞", body: "九州工業大学 情報工学部 情報・通信工学科"},
+    {date: "6.2019", name: "CTF ビギナーズ for 高専生", body: "Offensive Securityの基礎"},
+    {date: "3.2022", name: "電子情報通信学会九州支部成績優秀賞", body: "九州工業大学 情報工学部 情報・通信工学科"},
 
   ]
-
-  const drawer =  (contents.map((e) => (
-      <Event interval={e.date} title={e.title} subtitle={e?.subtitle}>
-        {e.body}
-      </Event>
-    ))
-  );
 
   return (
     <div>
       <Title name={"Activities"} />
-      <Timeline lineColor="black">
-        {drawer}
-      </Timeline>
+      <List sx={{ witdh: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+        {
+          rows.map( (e) => (
+            <ListItem>
+              <ListItemText primary={e.name} secondary={buildActivity(e.body, e.date)} />
+            </ListItem>
+          ))
+        }
+      </List>
     </div>
   );
 }
 
+function buildActivity(body, date) {
+  return <span>{body} <br /> {date} </span>;
+}
 export default History;
