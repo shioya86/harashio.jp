d55735e42fb7254b42805300d43be12f464ae351
Shoya HARA <shara@ml.harashio.jp>
2022-12-22 14:24:00 +0900

    AP受かった

--- src/pages/Qualifications.js
+++ src/pages/Qualifications.js
@@ -18,7 +18,8 @@ function Qualifications() {
     {date: "5.2019", name: "基本情報技術者資格"},
     {date: "3.2020", name: "準学士(工学) - 都城工業高等専門学校"},
     {date: "3.2022", name: "学士(情報工学) - 九州工業大学"},
-    {date: "8.2022", name: "三級知的財産管理技能検定"}
+    {date: "8.2022", name: "三級知的財産管理技能検定"},
+    {date: "2022", name: "応用情報技術者試験書"}
   ]
   return (
     <Card style={styles.card}>
