4d816ad0f12caec34720e443ee0c00153c75a29d
shara <shara@ml.harashio.jp>
2023-11-21 03:18:58 +0900

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 import {AppBar, Toolbar, Typography} from "@material-ui/core";
 
@@ -25,17 +24,12 @@ const styles = {
 
 root.render(
     <React.StrictMode>
-        <AppBar position="static" style={styles.header}>
-            <Toolbar>
-                <Typography>Shioya86's Portfolio</Typography>
-            </Toolbar>
-        </AppBar>
+      <AppBar position="static" style={styles.header}>
+          <Toolbar>
+              <Typography>Shioya86's Portfolio</Typography>
+          </Toolbar>
+      </AppBar>
       <App />
       <footer style={styles.footer}> Copyright © harashio.jp </footer>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
